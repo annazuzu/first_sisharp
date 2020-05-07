5b09e94b8375e611d4b4eb5a02452aa3e0ac8018
annazuzu <maxann89@gmail.com>
2020-05-07 17:08:04 +0300

    Переделан тест, так как текст старого кода оказался нерабочим.

--- csharp-example/csharp-example/UnitTest1.cs
+++ csharp-example/csharp-example/UnitTest1.cs
@@ -1,40 +1,44 @@
 using System;
-using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
 
 namespace csharp_example
 {
+    [TestFixture]
     public class MyFirstTest
     {
 
         private IWebDriver driver;
         private WebDriverWait wait;
+        private bool acceptNextAlert = true;
 
         [SetUp]
         public void start()
         {
             driver = new ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
         }
 
         [Test]
+        [Obsolete]
         public void FirstTest()
         {
             driver.Url = "https://www.google.com/";
             driver.FindElement(By.Name("q")).SendKeys("webdriver");
-            driver.FindElement(By.Name("btnK")).Click();
-            wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+            driver.FindElement(By.Id("tsf")).Submit(); // нажимаем "Enter" на клавиатуре
+            wait.Until(condition: ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
         }
 
         [TearDown]
-
-        public void stop() {
+        protected void stop()
+        {
             driver.Quit();
             driver = null;
-        
-        } 
+        }
 
     }
+
 }
